bfbd3bee80d94d307db32dfda14c299a54a1971f
antoniocra04 <antoniocra04@gmail.com>
2024-02-14 09:42:32 +0700

    feature: add db

---
+++ oop-backend/Context/DBContext.cs
@@ -0,0 +1,24 @@
+﻿namespace oop_backend.Context
+{
+    using Microsoft.EntityFrameworkCore;
+    using oop_backend.Models;
+
+    public class DBContext
+        : DbContext
+    {
+        public DBContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>().HasAlternateKey(i => i.Id);
+            modelBuilder.Entity<Customer>().HasAlternateKey(c => c.Id);
+        }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
