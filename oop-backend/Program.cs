aa0f30bc028d60b46d837db0ea956a09c70d10f4
antoniocra04 <antoniocra04@gmail.com>
2024-02-14 18:20:23 +0700

    chore

--- oop-backend/Program.cs
+++ oop-backend/Program.cs
@@ -1,3 +1,6 @@
+using oop_backend.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DBContext>(options => options.UseInMemoryDatabase("oop-back"));
 
 var app = builder.Build();
 
