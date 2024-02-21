098fbb773ba66dda6601f7d782f85044c8e09782
Anton Ryazanov <antoniocra04@gmail.com>
2024-02-21 16:32:27 +0700

    feature(models): add CategoryType

---
+++ oop-backend/Models/CategoryType.cs
@@ -0,0 +1,17 @@
+﻿namespace oop_backend.Models
+{
+    /// <summary>
+    /// Категории товаров
+    /// </summary>
+    public enum CategoryType
+    {
+        Book,
+        Laptop,
+        Phone,
+        Hardware,
+        Storage,
+        Headphone,
+        Cabel,
+        Fuel
+    }
+}
