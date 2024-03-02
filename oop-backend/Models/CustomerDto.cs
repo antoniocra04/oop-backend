b5fe0dbe5113b8c8052897dc404f654f8d09f405
Anton Ryazanov <antoniocra04@gmail.com>
2024-03-02 15:12:28 +0700

    fix(sln): правки

---
+++ oop-backend/Models/CustomerDto.cs
@@ -0,0 +1,55 @@
+﻿using oop_backend.Models.Utils;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace oop_backend.Models
+{
+    /// <summary>
+    /// Хранит данные покупателя получаемые в запросе
+    /// </summary>
+    public class CustomerDto
+    {
+        /// <summary>
+        /// Создает экземпляр класса <see cref="CustomerDto"/>.
+        /// </summary>
+        /// <param name="fullname">Полное имя.</param>
+        /// <param name="address">Адрес.</param>
+        /// <param name="Id">Id.</param>
+        public CustomerDto(string fullname, Address address, int id)
+        {
+            this.Id = id;
+            this.Fullname = fullname;
+            this.Address = address;
+        }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="CustomerDto"/>.
+        /// </summary>
+        /// <param name="fullname">Полное имя.</param>
+        /// <param name="address">Адрес.</param>
+        [JsonConstructor]
+        public CustomerDto(string fullname, Address address)
+        {
+            this.Id = IdGenerator.GetId();
+
+            this.Fullname = fullname;
+            this.Address = address;
+        }
+
+        /// <summary>
+        /// Id покупателя.
+        /// </summary>
+        public int Id { get; }
+
+        /// <summary>
+        /// Полное имя покупателя.
+        /// </summary>
+        [StringLength(200, ErrorMessage = "Имя должно быть меньше 200 символов")]
+        public string Fullname { get; set; }
+
+        /// <summary>
+        /// Адрес покупателя.
+        /// </summary>
+        public Address Address { get; set; }
+    }
+}
